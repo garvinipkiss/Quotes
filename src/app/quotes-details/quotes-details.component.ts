4b586460c6728c09d7a1bbd3782289640ce04632
garvinipkiss <garvinipkiss@gmail.com>
2018-03-18 23:01:48 +0300

    progressing

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit,Input,Output,EventEmitter} from '@angular/core';
-import { Quotes } from '../quotes'
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote-details',
-  templateUrl: './quotes-details.component.html',
-  styleUrls: ['./quotes-details.component.css']
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
 })
-export class QuotesDetailsComponent implements OnInit {
-@Input() quotes:Quotes;
+export class QuoteDetailsComponent implements OnInit {
+@Input() quote:Quote;
 @Output() isComplete= new EventEmitter<boolean>();
 
  quoteDelete(complete:boolean){
