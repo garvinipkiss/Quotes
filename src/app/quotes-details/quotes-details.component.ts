a5ec637aba030d4b8a1647aa1dccd2b77372226b
garvinipkiss <garvinipkiss@gmail.com>
2018-03-18 20:48:37 +0300

    progress

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -6,3 +6,16 @@ import { Quote } from '../quote'
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
+export class QuoteDetailsComponent implements OnInit {
+@Input() quote:Quote;
+@Output() isComplete= new EventEmitter<boolean>();
+
+ quoteDelete(complete:boolean){
+   this.isComplete.emit(complete);
+ }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
