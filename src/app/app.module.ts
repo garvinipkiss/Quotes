bbc9800695cca815e4ebc1eb89501dda13c97b13
garvinipkiss <garvinipkiss@gmail.com>
2018-03-18 20:16:47 +0300

    progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule
