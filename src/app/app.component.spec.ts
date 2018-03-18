8190dfa1b00f3fe2350562e8d74140939af5b571
garvinipkiss <garvinipkiss@gmail.com>
2018-03-18 21:48:45 +0300

    progressing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -8,12 +9,12 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ QuoteComponent ]
-    })
-    .compileComponents();
-  }));
+
+  it('should create the app', async(() => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.debugElement.componentInstance;
+  expect(app).toBeTruthy();
+}));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(QuoteComponent);
