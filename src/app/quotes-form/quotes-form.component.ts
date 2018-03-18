567514b6f4196a72471ca2b30f2f66179ad88285
garvinipkiss <garvinipkiss@gmail.com>
2018-03-18 21:31:51 +0300

    progressing

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
-  selector: 'app-quotes-form',
-  templateUrl: './quotes-form.component.html',
-  styleUrls: ['./quotes-form.component.css']
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
 })
-export class QuotesFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
